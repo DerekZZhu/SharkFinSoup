0750ba871adf4143e1cb0ad7d9cb83869454d990
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-09-15 22:06:21 -0700

    Added Bollinger Bands function. Tested: Works!

--- code/indicators.py
+++ code/indicators.py
@@ -9,7 +9,7 @@ from scraper import get_single
 - MACD helps investors understand whether the bullish or bearish movement in the price is strengthening or weakening.
 '''
 def MACD(dataset):
-    df = pd.DataFrame.copy(dataset)
+    df = dataset.copy()
     EMA_26 = df['Adj Close'].ewm(span=26, min_periods=26).mean()
     EMA_12 = df['Adj Close'].ewm(span=12, min_periods=12).mean()
 
@@ -19,12 +19,31 @@ def MACD(dataset):
     return df
 
 
+'''
+- The average true range (ATR) is a market volatility indicator used in technical analysis.
+- It is typically derived from the 14-day simple moving average of a series of true range indicators.
+- The ATR was originally developed for use in commodities markets but has since been applied to all types of securities.
+'''
 def ATR(dataset, n=14):
-    df = pd.DataFrame.copy(dataset)
+    df = dataset.copy()
     df['ATR'] = np.maximum.reduce([
         df['High'] - df['Low'],
         abs(df['High'] - df['Adj Close'].shift(1)),
         abs(df['Low'] - df['Adj Close'].shift(1))
     ])
-    
+
     return df['ATR'].ewm(com=n, min_periods=n).mean()
+
+
+def BB(dataset, n=20):
+    df = dataset.copy()
+    df['Middle Band'] = df["Adj Close"].rolling(window=n).mean()
+    std = df["Adj Close"].rolling(window=n).std(ddof=0)
+
+    df['Upper Band'] = df['Middle Band'] + 2*std
+    df['Lower Band'] = df['Middle Band'] - 2*std
+
+    return df[['Middle Band', 'Upper Band', 'Lower Band']]
+
+df = get_single('AMZN')
+print(BB(df).tail())
